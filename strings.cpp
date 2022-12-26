90d105f29c98007e6677b8737b821349ac07a119
Tomer Nahum <github@tomer.nahum.org>
2022-12-26 04:05:46 -0500

    learning about string literals

--- strings.cpp
+++ strings.cpp
@@ -8,8 +8,7 @@ Strings (literal strings not std strings)
 (can not be edited)
 */
 int main() {
-    "Hello"; 
-    //What a string literal really means is a const char array (hover over it in editor)
+    "Hello";  //What a string literal really means is a const char array (hover over it in editor)
     // length is 1 more than num characters because of null terminator thing at end ( written as \0)
     std::cout << "Hel\0lo" << std::endl; // \0 is terminator. dont do this probably
     
@@ -38,22 +37,37 @@ int main() {
     /*
     - Not allowed to have non-const string char array pointer 
     (undefined behavior, inconsistent across compilers, some won't let you compile)
-    
+    if you try to modify it you'll get a segmentation fault maybe? anyway you cant write there so
+
+    char ttest[] = "Hello"; //I think its undefined behavior here
+    ttest[4] = '!'; 
+
+
     - if you want a mutable char array you can use []
-
-    char ttest[] = "Hello";
-    ttest[4] = "!"; //in the compiler im using g++ it puts an error here
     */
-
+    //if you want a mutable char array you can use a regular array {}
     char mTest[] = {'O','M','G','\0'};
     LOG(mTest);
     mTest[2] = 'M';
     LOG(mTest);
 
-
-
     LOG("\n"<< "Hello"[4]); //reading works you just cant write
     
+
+
+    
+    
+    LOG("");
+    /*
+    There are a few types of chars and strings for different encoders / bit sizes
+    not really important to learn right now imo 
+    */
+    const char* x = "Hello"; // 1byte/char                  //UTF-8
+    const wchar_t* y = L"Hello";
+
+    const char16_t* z = u"Hello"; // 2 byte(16bit)/char ()  //UTF-16
+    const char32_t* a = U"Hello"; // 4 bytes(32bit)/char    //UTF-32
+
 
 
     return 0;

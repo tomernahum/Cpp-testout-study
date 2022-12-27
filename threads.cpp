56f962960d5ae2d7f62d7a7f00637ae5ed69ca54
Tomer Nahum <github@tomer.nahum.org>
2022-12-27 01:51:41 -0500

    learning threads basics

--- threads.cpp
+++ threads.cpp
@@ -4,12 +4,16 @@
 #define print(x) std::cout << x << std::endl
 
 
-// !!! You need to do -pthread when compiling dont ask me why i dont even need to be learning threads in the first place i am on time crunch
-// also try -lpthread idk
+// !!! You need to do -pthread when compiling dont ask me why i dont even need to be learning threads in the first place i am on time crunch //also try -lpthread idk
 
+bool is_finished = false;
 void DoWorkk()
 {
-    std::cout << "Hello";
+    while (!is_finished){
+        std::cout << "Hello\n";
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 }
 
 int main()
@@ -17,13 +21,13 @@ int main()
     std::thread workerr(DoWorkk); //aka    thread workerrr = thread(DoWorkk);
     //^^ this immediatley starts the thread running the function
 
-    //std::string x;
-    //std::cin >> x;
-    
+    std::string x;
+    std::cin >> x; //seems to wait for you to enter something besides nothing
+    is_finished = true;
     
     workerr.join(); // aka wait for workerr to complete before continuing main thread
     
-    
-    
+    std::cout << "Bye";
+
     return 0;
 }
